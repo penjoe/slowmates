88cf7df87edc3f00548b452e424662c8084dbbee
Joe Pennock <pennockjoe@gmail.com>
2022-03-08 18:52:27 -0800

    update team member, placeholder link for OMM link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
   return(
     <div className="footer">
       <h3>&copy; Slowmates - All Rights Reserved</h3>
+      <h4>Powered by <a href="https://discord.gg/ktw4Pz5V5H">OMM</a></h4>
     </div>
   );
 };
