3237e314b9e521be61978e604707d23ae8518cfc
Joe Pennock <pennockjoe@gmail.com>
2022-05-25 18:15:58 -0700

    work on nav responsive

---
+++ src/components/Hamburger.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hamburger = () => {
+  return(
+    <div className='hamburger'>
+      <div className='burger burger 1' />
+      <div className='burger burger 2' />
+      <div className='burger burger 3' />
+    </div>
+  )
+}
+
+export default Hamburger;
