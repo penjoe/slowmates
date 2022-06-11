4c8f541dd63b09a00a043f8d94b0518b9b52c3be
Joe Pennock <pennockjoe@gmail.com>
2022-06-10 21:35:17 -0700

    updated team section, improved responsive layout

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,20 +1,51 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React from "react";
 
-import TeamCard from './TeamCard.js';
-import { IMAGES } from '../assets';
+import TeamCard from "./TeamCard.js";
+import { IMAGES } from "../assets";
 
 const Team = () => {
-  return(
+  return (
     <div className="team-wrap">
-      <a id='team' />
-      <h2>Meet the Slowmates Team</h2>
+      <a id="team" />
+      <h1>Meet the Slowmates Team</h1>
       <div className="team">
-        <TeamCard img={IMAGES.masterginge} twitter={'https://twitter.com/Masterginge27'} name={'@Masterginge'}/>
-        <TeamCard img={IMAGES.steelneil} twitter={'https://twitter.com/Steelneil_'} name={'@SteelNeil.NFT'}/>
-        <TeamCard img={IMAGES.nichNotNick} twitter={'https://twitter.com/RetroRecordss'} name={'@nich.eth'}/>
-        <TeamCard img={IMAGES.karan} twitter={'https://twitter.com/KaranKbKb'} name={'@KaranKbKb'}/>
+        <TeamCard
+          img={IMAGES.team3}
+          placement={1}
+          twitter={"https://twitter.com/CODENAME883"}
+          name={"@Codename"}
+          role={"Co-Founder"}
+        />
+        <TeamCard
+          img={IMAGES.team2}
+          placement={2}
+          twitter={"https://twitter.com/darwinite24"}
+          name={"@Darwinite"}
+          role={'Co-Founder'}
+        />
+        <TeamCard
+          img={IMAGES.steelneil}
+          placement={3}
+          twitter={"https://twitter.com/Steelneil_"}
+          name={"@SteelNeil.NFT"}
+          role={'Marketing'}
+        />
+        <TeamCard
+          img={IMAGES.masterginge}
+          placement={4}
+          twitter={"https://twitter.com/Masterginge27"}
+          name={"@masterginge"}
+          role={'Community Manager'}
+        />
+        <TeamCard
+          img={IMAGES.karan}
+          placement={5}
+          twitter={"https://twitter.com/KaranKbKb"}
+          name={"@KaranKbKb"}
+          role={'Intern'}
+        />
       </div>
     </div>
   );
--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const TeamCard = ({ img, name, twitter }) => {
+const TeamCard = ({ img, name, twitter, placement, role }) => {
   return(
-    <div className="team-card">
+    <div className={`team-card team-card-${placement}`}>
+      <img src={img} alt="" />
       <a href={twitter}>
         <h3>{name}</h3>
       </a>
-      <img src={img} alt="" />
+      <p>{role}</p>
     </div>
   );
 };
